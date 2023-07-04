1dd4a124c30ae21bb74d6c14c4b2b67c44bc4d75
Vinson Huang <vinsonh789@gmail.com>
2023-07-04 15:04:18 -0500

    initial commit

---
+++ src/Lab11.ino
@@ -0,0 +1,50 @@
+#include "LIS3DH.h"
+#include "oled-wing-adafruit.h"
+
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
+
+#define CSPIN D3
+
+OledWingAdafruit display;
+LIS3DHSPI accel(SPI, CSPIN, WKP);
+
+void setup()
+{
+    Serial.begin(9600);
+    while (!Serial.isConnected())
+    {
+    }
+
+    display.setup();
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.display();
+
+    LIS3DHConfig config;
+    config.setAccelMode(LIS3DH::RATE_100_HZ);
+    bool setupSuccess = accel.setup(config);
+    Serial.printlnf("setupSuccess=%d", setupSuccess);
+}
+
+void loop()
+{
+    display.loop();
+    LIS3DHSample sample;
+    // Checks if there is a valid sample
+    if (accel.getSample(sample))
+    {
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        // Display X, Y, and Z values from the sample
+        display.print("X: ");
+        display.println(sample.x);
+        display.print("Y: ");
+        display.println(sample.y);
+        display.print("Z: ");
+        display.println(sample.z);
+        display.display();
+    }
+}
